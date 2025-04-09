557470221694beec59e1c256702651b0a1d062ca
adhayv <adhayv@gmail.com>
2025-04-09 14:54:04 -0400

    Setup base react project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Sidebar } from './components/Sidebar'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="flex h-screen">
+        <Sidebar/>     
+        <div className="flex-1 p-4">
+          <h1 className="">AV</h1>
+          asdasdasdasd
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export function Sidebar() {
+
+    return(
+        <div className="fixed top-0 left-0 h-screen w-20 bg-gray-800 text-white flex flex-col items-center p-4">
+            <p className="text-lg font-bold">Menu</p>
+        </div>
+    )
+};
